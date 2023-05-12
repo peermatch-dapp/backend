8ee726ddb1c54e4f915fba3cce0d90045b429dfc
Didier Krux <didier@krux.co>
2023-05-13 00:05:13 +0100

    getInterests

--- src/routes/interestsRouter.ts
+++ src/routes/interestsRouter.ts
@@ -4,11 +4,24 @@ import { LENS_API_PROD } from "../config";
 
 const lens = new GraphQLClient(LENS_API_PROD);
 
-export async function getInterests(profileId: string) {
-  const query = gql`
+const profileId = "0x0e29"
+
+export async function getInterests() {
+  const myInterestsQuery = `query Profile {
+    profile(request: { profileId: "0x0e29" }) {
+      id
+      interests
+    }
+  }`
+
+  const { profile: { interests } }: any = await lens.request(myInterestsQuery);
+
+  console.log(interests)
+
+  const followersQuery = gql`
   query Followers {
     followers(request: { 
-                  profileId: "0x0e29",
+                profileId: "0x0e29",
                 limit: 50
                }) {
          items {
@@ -24,15 +37,24 @@ export async function getInterests(profileId: string) {
     }
   }
   `;
-  const data = await lens.request(query);
-  console.log(data)
+  const { followers: { items } }: any = await lens.request(followersQuery);
+  console.log(items)
+  const data = []
+  for (const item of items) {
+    console.log(item.wallet.defaultProfile.interests)
+    const commonInterests = item.wallet.defaultProfile.interests.filter((element: any) => {
+      return interests.includes(element);
+    });
+    console.log(commonInterests)
+    data.push({ address: item.wallet.address, commonInterests, numberInCommon: commonInterests.length, name: item.wallet.defaultProfile.name })
+  }
   return data;
 }
 
 const interestsRouter = Router();
 
 interestsRouter.get('/', async (req, res) => {
-  const interests = await getInterests('32751')
+  const interests = await getInterests()
   res.json(interests)
 })
 
